ffaf9a931ce723054e104b235eb0c34fdfd1c864
Elijah Falope <elijah.falope@gmail.com>
2019-11-22 16:20:43 -0500

    11/22/2019 Update

---
+++ solcast_api_work.py
@@ -0,0 +1,57 @@
+import requests
+import json
+import pandas as pd
+
+api_base = 'https://api.solcast.com.au'
+
+solcast_api_key =  "U7mG4zUHGW3gczhlcZslxE2QiWDOTGJl"
+#api_key = "U7mG4zUHGW3gczhlcZslxE2QiWDOTGJl"
+
+statuscode = 0
+
+# Accessing SolCast API for  World Solar radiation Data at point Content should include estimated actuals
+
+def world_sol_radiation_data_api():
+
+  response = requests.get(
+
+                "https://api.solcast.com.au/world_radiation/estimated_actuals.json",
+                params = {
+                    'api_key':solcast_api_key,
+                    'latitude':33.895,
+                     'longitude': -118.308}
+                     ) # Specify Longitude, langitude
+
+
+  statuscode = response.status_code
+  api_content = response.json()
+  print(statuscode)
+  #print(response.content)
+  print(api_content)
+
+# Accessing Solcast API for Rooftop Sites at point Content should include estimated actuals
+# Curently returns 404
+def rooftop_sites():
+
+  response = requests.get(
+       "https://api.solcast.com.au/rooftop_sites/{a6b0-f9cc-4e61-89c5}/estimated_actuals.json",
+       params = {
+           'api_key': solcast_api_key,
+           'latitude':33.895,
+           'longitude': -118.308,
+           'capacity': 5,
+           'tilt': 23,
+           'azimuth' : 0,
+           'loss_factor' : 0.9
+           })
+  statuscode = response.status_code
+  api_content = response.json()
+  print(statuscode)
+  #print(response.content)
+  print(api_content)
+
+
+
+
+world_sol_radiation_data_api()
+rooftop_sites()
