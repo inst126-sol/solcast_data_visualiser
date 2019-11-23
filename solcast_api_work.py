e25398b422446388f0d14ccb35774db100afa08a
headedhorseman101 <jpool.school@gmail.com>
2019-11-23 01:54:44 -0500

    added dictionary conversion algorithm

--- solcast_api_work.py
+++ solcast_api_work.py
@@ -50,8 +50,26 @@ def rooftop_sites():
   #print(response.content)
   print(api_content)
 
+# get data from api
+raw_world_data = world_sol_radiation_data_api()['estimated_actuals']
+raw_rooftop_data = rooftop_sites()['estimated_actuals']
 
+# convert json data into dictionary readable by panda - bring over to main branch
+def convert_data(raw_data):
+    data = {}
+    # initialize key-value pairs as empty lists
+    for variable in raw_data[0]:
+        data[variable] = []
+    # restructure raw data into dictionary with variable labels as keys and data as values
+    for i in range(len(raw_data)):
+        for variable in raw_data[i]:
+            list = data[variable]
+            list.append(raw_data[i][variable])
+    # construct dataframe
+    df = pd.DataFrame.from_dict(data)
+    return df
 
+world_df = convert_data(raw_world_data)
+rooftop_df = convert_data(raw_rooftop_data)
 
-world_sol_radiation_data_api()
-rooftop_sites()
+print(world_df.head(25))
