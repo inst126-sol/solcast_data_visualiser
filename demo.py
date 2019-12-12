fc6d03c07c05d223ad847c0886d4b6f4f9c981bb
Jonah Pool <jpool.school@gmail.com>
2019-12-11 21:55:26 -0500

    final update

--- demo.py
+++ demo.py
@@ -4,65 +4,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-api_base = 'https://api.solcast.com.au'
-solcast_api_key =  "Zr4AINd9PudrH8TRJTV2AwIKa7giqYQf"
-#api_key = "Zr4AINd9PudrH8TRJTV2AwIKa7giqYQf"
-
-statuscode = 0
-
-address = input("Write the address you are looking up (ex. 12345 Oxhammer Rd, New York, NY 67890, USA): ")
-#while (address.lower().strip() is not "done") and (requested_data is not "done"):
-delimiter = ' '
-address_parts = address.split(", ")
-street_address = address_parts[0].split()
-house_number = str(street_address[0])
-street = delimiter.join(street_address[1:]).rstrip(",")
-city = address_parts[1]
-state_zip = address_parts[2].split()
-#state = state_zip[0].upper()
-#zip_code = state_zip[1]
-country =
-
-print(house_number)
-print(street)
-print(city)
-print(state)
-print(zip_code)
-
-#house_number = input("Enter a house or street number: ")g
-#street = input("Enter a street name: ")
-#city =  input("Enter city name: ")
-#country = input("Enter the country address: ")
-
-
 def here_call(house_number,street,city,country):
 
-    #app_id and app_code are paramters form the Here project website
-  app_id = 'zI8FECxjf9LGliewQa5u'
-  app_code = 'okD8RNAk-DkxBkisBguzpg'
+#app_id and app_code are paramters form the Here project website
+    app_id = 'zI8FECxjf9LGliewQa5u'
+    app_code = 'okD8RNAk-DkxBkisBguzpg'
 
-  #API GET REQUESTS
-  here_response = requests.get("https://geocoder.api.here.com/6.2/geocode.json", params ={"app_id": app_id,"app_code":app_code, 'housenumber': house_number, 'street':street, "city":city, "zip":zip_code} )
-
-  #NOTE 2 SELF : ADD RESPONSE IF API CALL IS BAD / NOT 200
-  status = here_response.status_code
-  if status == 200:
+    #API GET REQUESTS
+    here_response = requests.get("https://geocoder.api.here.com/6.2/geocode.json", params ={"app_id": app_id,"app_code": app_code, "housenumber": house_number, "street":street, "city": city, "country": country})
+    #print(here_response)
+    status = here_response.status_code
     api_content = here_response.json()
+    if status != 200:
+        print("The address you enter was not found. This may be to a incorrect spelling! Try again")
 
-    if len(api_content) == 0:
-      print("The address you enter was not found. This may be to a incorrect spelling! Try again")
-
-  else:
-    locationinfo = api_content["Response"]["View"][0]["Result"][0]
-    latitude = locationinfo['Location']['DisplayPosition']['Latitude']
-    longitude = locationinfo['Location']['DisplayPosition']['Longitude']
-
-    return [latitude,longitude]
-
-#The Longitude and Laitiude for the address entered if HERE CODER API was successful
-
-latitude1 = here_call(house_number,street,city,country)[0]
-longitude1 = here_call(house_number,street,city,country)[1]
+    else:
+        locationinfo = api_content["Response"]["View"][0]["Result"][0]
+        latitude = locationinfo['Location']['DisplayPosition']['Latitude']
+        longitude = locationinfo['Location']['DisplayPosition']['Longitude']
+        return [latitude,longitude]
 
 # Accessing SolCast API for  World Solar radiation Data at point Content should include estimated actuals
 
@@ -84,27 +44,6 @@ def world_sol_radiation_data_api(longitude,latitude):
   #print(response.content)
   return api_content
 
-# Accessing Solcast API for Rooftop Sites at point Content should include estimated actuals
-
-def rooftop_sites():
-
-  response = requests.get(
-       "https://api.solcast.com.au/rooftop_sites/{a6b0-f9cc-4e61-89c5}/estimated_actuals.json",
-       params = {
-           'api_key': solcast_api_key,
-           'latitude':latiude1,
-           'longitude': longitude1,
-           'capacity': 5,
-           'tilt': 23,
-           'azimuth' : 0,
-           'loss_factor' : 0.9
-           })
-  statuscode = response.status_code
-  api_content = response.json()
-  #print(statuscode)
-  #print(response.content)
-  return api_content
-
 def world_pv_power():
   response = requests.get(
        "https://api.solcast.com.au/world_pv_power/estimated_actuals.json",
@@ -116,15 +55,9 @@ def world_pv_power():
        }
     )
   statuscode = response.status_code
-  print(statuscode)
+  #print(statuscode)
   api_content = response.json()
   return api_content
-
-
-# get data from api
-raw_world_data = world_sol_radiation_data_api(longitude1,latitude1)['estimated_actuals']
-raw_rooftop_data = rooftop_sites()['estimated_actuals']
-raw_worldpv_data = world_pv_power()['estimated_actuals']
 
 # convert json data into dictionary readable by panda - bring over to main branch
 def convert_data(raw_data):
@@ -141,11 +74,6 @@ def convert_data(raw_data):
   df = pd.DataFrame.from_dict(data)
   return df
 
-world_df = convert_data(raw_world_data)
-rooftop_df = convert_data(raw_rooftop_data)
-worldpvdf = convert_data(raw_worldpv_data)
-
-# create a list of times from 'period_end' - these will be our axis labels
 def time_labels(data_frame):
     times = []
     for i in range(len(data_frame)):
@@ -156,25 +84,122 @@ def time_labels(data_frame):
         times.append(time)
     return times
 
-world_df['period_end_time'] = time_labels(world_df)
+def plot_ghi():
+    # plot world_df as lineplot
+    plt.plot('period_end','ghi', data=world_df)
+    plt.title('ghi: estimated actual')
+    plt.xlabel('Time (GMT+0)')
+    plt.ylabel('ghi (W/m^2)')
+    plt.xticks(world_df['period_end'], world_df['period_end_time'], rotation = 'vertical')
+    plt.subplots_adjust(right=3, top = 1)
+    plt.show()
 
-# plot world_df as lineplot
-plt.plot('period_end','ghi', data=world_df)
-plt.title('ghi: estimated actual')
-plt.xlabel('Time (GMT+0)')
-plt.ylabel('ghi')
-plt.xticks(world_df['period_end'], world_df['period_end_time'], rotation = 'vertical')
-plt.subplots_adjust(right=3, top = 1)
-plt.show()
+def plot_ebh():
+    # plot world_df as lineplot
+    plt.plot('period_end','ebh', data=world_df)
+    plt.title('ebh: estimated actual')
+    plt.xlabel('Time (GMT+0)')
+    plt.ylabel('ebh (W/m^2)')
+    plt.xticks(world_df['period_end'], world_df['period_end_time'], rotation = 'vertical')
+    plt.subplots_adjust(right=3, top = 1)
+    plt.show()
+
+def plot_dhi():
+    # plot world_df as lineplot
+    plt.plot('period_end','dhi', data=world_df)
+    plt.title('dhi: estimated actual')
+    plt.xlabel('Time (GMT+0)')
+    plt.ylabel('dhi (W/m^2)')
+    plt.xticks(world_df['period_end'], world_df['period_end_time'], rotation = 'vertical')
+    plt.subplots_adjust(right=3, top = 1)
+    plt.show()
+
+def plot_dni():
+    # plot world_df as lineplot
+    plt.plot('period_end','dni', data=world_df)
+    plt.title('dni: estimated actual')
+    plt.xlabel('Time (GMT+0)')
+    plt.ylabel('dni (W/m^2)')
+    plt.xticks(world_df['period_end'], world_df['period_end_time'], rotation = 'vertical')
+    plt.subplots_adjust(right=3, top = 1)
+    plt.show()
+
+def plot_pv_estimate():
+    # plot rooftop as lineplot
+    plt.plot('period_end','pv_estimate', data=worldpv_df)
+    plt.title('pv estimate (): estimated actual')
+    plt.xlabel('Time (GMT+0)')
+    plt.ylabel('pv estimate')
+    plt.xticks(worldpv_df['period_end'], worldpv_df['period_end_time'], rotation = 'vertical')
+    plt.subplots_adjust(right=3, top = 1)
+    plt.show()
 
 
-rooftop_df['period_end_time'] = time_labels(rooftop_df)
 
-# plot rooftop as lineplot
-plt.plot('period_end','pv_estimate', data=rooftop_df)
-plt.title('pv_estimate: estimated actual')
-plt.xlabel('Time (GMT+0)')
-plt.ylabel('pv_estimate')
-plt.xticks(rooftop_df['period_end'], rooftop_df['period_end_time'], rotation = 'vertical')
-plt.subplots_adjust(right=3, top = 1)
-plt.show()
+# global variable definitions
+
+api_base = 'https://api.solcast.com.au'
+solcast_api_key =  "Zr4AINd9PudrH8TRJTV2AwIKa7giqYQf"
+#api_key = "Zr4AINd9PudrH8TRJTV2AwIKa7giqYQf"
+
+statuscode = 0
+
+i = 0
+address = input("Write the address you are looking up (ex. 12345 Oxhammer, New York, NY 67890, USA): ")
+
+# main code
+while (address.lower().strip() is not "done") and (i < 5):
+    delimiter = ' '
+
+    address_parts = address.split(", ")
+    street_address = address_parts[0].split()
+    house_number = int(street_address[0])
+    street = delimiter.join(street_address[1:]).rstrip(",").lower()
+    city = address_parts[1].lower()
+    #state_zip = address_parts[2].split()
+    #state = state_zip[0].upper()
+    #zip_code = state_zip[1]
+    country = address_parts[3].lower()
+
+    #print(house_number)
+    #print(street)
+    #print(city)
+    #print(country)
+
+    #The Longitude and Laitiude for the address entered if HERE CODER API was successful
+
+    latitude1 = here_call(house_number,street,city,country)[0]
+    longitude1 = here_call(house_number,street,city,country)[1]
+
+    varnames = ['ghi', 'dhi','ebh','dni']
+
+    variable_request = input("Which radiation variable would you like to plot? (ghi/dhi/ebh/dni): ")
+    if variable_request == "done":
+      break
+    elif variable_request not in varnames:
+      print('Not a valid data set. %d attempts left.\n' % (5-i))
+    else:
+        # get data from api
+        raw_world_data = world_sol_radiation_data_api(longitude1,latitude1)['estimated_actuals']
+        raw_worldpv_data = world_pv_power()['estimated_actuals']
+
+        world_df = convert_data(raw_world_data)
+        worldpv_df = convert_data(raw_worldpv_data)
+
+        # create a list of times from 'period_end' - these will be our axis labels
+
+        world_df['period_end_time'] = time_labels(world_df)
+        worldpv_df['period_end_time'] = time_labels(worldpv_df)
+        if variable_request == "ghi":
+            plot_ghi()
+        elif variable_request == "ebh":
+            plot_ebh()
+        elif variable_request == "dhi":
+            plot_dhi()
+        elif variable_request == "dni":
+            plot_dni()
+        elif variable_request == "done":
+          break
+        plot_pv_estimate()
+        print('%d attempts left.' % (5-i))
+    i += 1
